9dae7912ce3a02b10ef49101c0866b8f90009554
ValeedAnjum <allinone644@gmail.com>
2021-08-22 12:15:40 +0500

    MiniPlayer videocard work

--- client/src/components/modelManager/miniPlayer/MiniPlayerFooter.js
+++ client/src/components/modelManager/miniPlayer/MiniPlayerFooter.js
@@ -1,13 +1,18 @@
 import { Grid, IconButton, makeStyles } from "@material-ui/core";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import React from "react";
+import { Fragment } from "react";
 import VideosList from "./VideosList";
 const title =
   "Christian Sister Accept Islam After She Got Her 2 Answer - Dr. Zakir Naik";
 const useStyles = makeStyles(() => {
   return {
-    miniPlayerFooter: {
-      // padding: "10px",
+    miniPlayertitleAndMore: {
+      padding: "10px",
+      overflow: "hidden",
+    },
+    moreVideoArrowIcon: {
+      padding: "3px",
     },
     videoTitle: {
       textOverflow: "ellipsis",
@@ -22,29 +27,49 @@ const useStyles = makeStyles(() => {
       color: "#767676de",
       margin: "0",
     },
+    vidoeItemContainer: {
+      padding: "0px 10px",
+    },
   };
 });
 const MiniPlayerFooter = () => {
   const classes = useStyles();
   return (
-    <Grid container className={classes.miniPlayerFooter}>
-      <Grid item container direction="column" sm={11}>
-        <Grid item>
-          <p className={classes.videoTitle}>{title}</p>
+    <Fragment>
+      <Grid container className={classes.miniPlayertitleAndMore}>
+        <Grid item container direction="column" sm={11}>
+          <Grid item className={classes.videoTitle}>
+            {/* <p className={classes.videoTitle}>{title}</p> */}
+            {title}
+          </Grid>
+          <Grid item>
+            <p className={classes.queue}>Queues. 2/9</p>
+          </Grid>
         </Grid>
-        <Grid item>
-          <p className={classes.queue}>Queues. 2/9</p>
+        <Grid item sm={1}>
+          <IconButton className={classes.moreVideoArrowIcon}>
+            <KeyboardArrowDownIcon />
+          </IconButton>
         </Grid>
       </Grid>
-      <Grid item sm={1}>
-        <IconButton>
-          <KeyboardArrowDownIcon />
-        </IconButton>
-      </Grid>
-      <Grid item container direction="column">
+      <Grid
+        item
+        container
+        direction="column"
+        className={classes.vidoeItemContainer}
+      >
+        <VideosList />
+        <VideosList />
+        <VideosList />
+        <VideosList />
+        <VideosList />
+        <VideosList />
+        <VideosList />
+        <VideosList />
+        <VideosList />
         <VideosList />
       </Grid>
-    </Grid>
+    </Fragment>
   );
 };
 
--- client/src/components/modelManager/miniPlayer/VideosList.js
+++ client/src/components/modelManager/miniPlayer/VideosList.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Grid, IconButton } from "@material-ui/core";
+import { Grid, IconButton, makeStyles } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 import DragHandleIcon from "@material-ui/icons/DragHandle";
 const title = "Christian Sister Accept Islam After She ...";
 const image =
   "https://i.ytimg.com/vi/T5ZlSSOhbzQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCE1GtvJ71DA7wgpdS9YMPDCNaloQ";
+const useStyles = makeStyles(() => {
+  return {
+    videoItem: {
+      paddingRight: "10px",
+    },
+    titleAndChannelName: {
+      paddingLeft: "5px",
+    },
+    title: {
+      fontWeight: "600",
+    },
+    channel: {
+      color: "#767676de",
+    },
+  };
+});
 const VideosList = () => {
+  const classes = useStyles();
   return (
-    <Grid item container wrap="nowrap">
+    <Grid item container wrap="nowrap" className={classes.videoItem}>
       <Grid item>
         <IconButton>
           <DragHandleIcon />
@@ -14,7 +33,7 @@ const VideosList = () => {
       </Grid>
       <Grid item container>
         <Grid item sm={3}>
-          <img src={image} width="100%" />
+          <img src={image} width="100%" height="100%" />
         </Grid>
         <Grid
           item
@@ -22,13 +41,18 @@ const VideosList = () => {
           container
           direction="column"
           justifyContent="space-between"
+          className={classes.titleAndChannelName}
         >
-          <Grid item>{title}</Grid>
-          <Grid item>Channel Name</Grid>
+          <Grid item className={classes.title}>
+            {title}
+          </Grid>
+          <Grid item className={classes.channel}>
+            Dr.Zakir
+          </Grid>
         </Grid>
         <Grid item sm={1}>
           <IconButton>
-            <DragHandleIcon />
+            <MoreVertIcon />
           </IconButton>
         </Grid>
       </Grid>
